31e9778534fd09da6f5235aa9d5c1d263327708c
dotansimha <dotansimha@gmail.com>
2016-07-07 09:41:43 +0300

    chore(tests): Base tests

---
+++ client/imports/demo/demo.component.test.ts
@@ -0,0 +1,10 @@
+import {DemoComponent} from "./demo.component";
+
+console.log(DemoComponent);
+
+describe('my module', function () {
+  it('does something that should be tested', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+  })
+});
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,5 @@
+/// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/meteor/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="modules/es6-promise/index.d.ts" />
