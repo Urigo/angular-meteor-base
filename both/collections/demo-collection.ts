3beec50f5b06c54e567ce8d65998bdc9b0683864
dotansimha <dotansimha@gmail.com>
2016-07-07 12:51:23 +0300

    Added README and fixed a little bug

--- both/collections/demo-collection.ts
+++ both/collections/demo-collection.ts
@@ -1,4 +1,4 @@
 import {DemoDataObject} from "../models/demo-data-object";
 import {Mongo} from "meteor/mongo";
 
-export const DemoCollection = new Mongo.Collection<DemoDataObject>();
+export const DemoCollection = new Mongo.Collection<DemoDataObject>('demo-collection');
